000df5ef23e200546cc198737e6c1f9ea81b647f
jyoti nayal <jyoti.nayal@cuelogic.com>
2021-01-20 15:50:51 +0530

    Added Add.c

---
+++ Add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void add(int a,int b)
+{
+    printf("%d",  a + b);
+}
+int main()
+{
+  //Initialize variable and rename
+   int first_num=0;
+   int second_num=0;
+
+    printf("Enter two operands: ");
+    scanf("%d %d", &first_num, &second_num);
+    add(first_num,second_num);
+    return 0;
+}
